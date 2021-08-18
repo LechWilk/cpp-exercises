a5b2c51f0fee219c66a8fda4aa87930dcc9d8df0
LechWilk <lech.wilk@gmail.com>
2021-08-18 07:34:46 +0200

    no access to fields of base class

--- shl/Fruit.hpp
+++ shl/Fruit.hpp
@@ -4,7 +4,7 @@
 class Fruit : public Cargo  {
     public:
         Fruit(int amount, std::string name, float basePrice, int timeToSpoil)
-        :   /*amount_(amount), name_(name), basePrice_(basePrice), */timeToSpoil_(timeToSpoil)
+        :   amount_(amount), name_(name), basePrice_(basePrice), timeToSpoil_(timeToSpoil)
         {}
         Fruit& operator--();
         virtual size_t getPrice() const = 0;
